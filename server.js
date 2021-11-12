e243cef621015eb31888c5667fe37587a261a331
CristobalHdz <cristobalhdzch@gmail.com>
2021-11-11 19:02:52 -0700

    fix server js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(logger("dev"))
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,14 +14,12 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
   useFindAndModify: false
 });
 
-// routes
-require("./routes/apiroute.js")(app);
-require("./routes/viewroute.js")(app);
+//require(apiRoute)(app);
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
