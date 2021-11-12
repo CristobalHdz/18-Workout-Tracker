212229d9facc9471de6aab06e3cb9f677f646d3b
CristobalHdz <cristobalhdzch@gmail.com>
2021-11-11 19:10:54 -0700

    Rewrite models folder

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger("dev"))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,12 +15,14 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
-//require(apiRoute)(app);
-require("./routes/viewroutes")(app);
-require("./routes/apiroutes")(app);
+// routes
+require("./routes/apiroute.js")(app);
+require("./routes/viewroute.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
